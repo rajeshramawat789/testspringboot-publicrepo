7d83d5b41ec6191933a98bb836430be25bb7e07b
rajeshramawat789 <rajeshramawat789@yahoo.in>
2021-05-23 14:02:01 +0530

    commit gradle

--- src/test/java/com/test/bdd/CucumberTest.java
+++ src/test/java/com/test/bdd/CucumberTest.java
@@ -11,7 +11,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        plugin = {"pretty", "json:target/cucumber-report.json"}
+        plugin = {"pretty", "json:target/cucumber-report.json"},
+        glue = {"com.test.bdd"}
         )
 public class CucumberTest extends CucumberSpringContextConfiguration{
 
